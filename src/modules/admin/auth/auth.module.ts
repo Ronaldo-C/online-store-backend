854fb2e74535a7537acd65c1d9003f31f9007e45
HuiWu <huiwuw@tripintl.com>
2025-05-28 19:56:39 +0800

    feat: add transform interceptor

--- src/modules/admin/auth/auth.module.ts
+++ src/modules/admin/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import { UsersModule } from '../users/users.module';
 
 @Global()
 @Module({
   imports: [
+    UsersModule,
     JwtModule.registerAsync({
       global: true,
       inject: [ConfigService],
