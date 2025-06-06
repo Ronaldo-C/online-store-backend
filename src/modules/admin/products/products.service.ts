ffe9508330a02fcfa201af6e74e16c535d3c02a6
HuiWu <huiwuw@tripintl.com>
2025-06-07 07:53:26 +0800

    feat: add update product logic

--- src/modules/admin/products/products.service.ts
+++ src/modules/admin/products/products.service.ts
@@ -1,33 +1,37 @@
-import { ConflictException, Inject, Injectable, Scope } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
 import { REQUEST } from '@nestjs/core';
 import { PrismaService } from 'src/shared/prisma/prisma.service';
 import { TRequest } from 'src/typeDefs/request';
-import { AuthService } from '../auth/auth.service';
 import { CreateProductDto } from './dto/create-product.dto';
-import { ERROR_CONFLICT_MESSAGE_CODE } from 'src/typeDefs/error-code';
+import {
+  ERROR_BAD_REQUEST_MESSAGE_CODE,
+  ERROR_CONFLICT_MESSAGE_CODE,
+} from 'src/typeDefs/error-code';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { isEmpty } from 'lodash';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class ProductsService {
   constructor(
     @Inject(REQUEST) private readonly request: TRequest,
     private readonly prisma: PrismaService,
-    private readonly authService: AuthService,
   ) {}
 
   async create(dto: CreateProductDto) {
-    if (dto.number) {
-      const existNumber = await this.prisma.product.findFirst({
-        where: {
-          number: dto.number,
-          deletedAt: null,
-        },
-      });
-      if (existNumber) {
-        throw new ConflictException(
-          `number|${ERROR_CONFLICT_MESSAGE_CODE.DUPLICATE_SLUG}`,
-        );
-      }
+    await this.validateParams(dto);
+
+    if (isEmpty(dto.skus)) {
+      throw new BadRequestException(
+        `skus|${ERROR_BAD_REQUEST_MESSAGE_CODE.EMPTY_ARRAY}`,
+      );
     }
+
     const product = await this.prisma.product.create({
       data: {
         name: dto.name,
@@ -59,6 +63,68 @@ export class ProductsService {
     return this.detail(product.id);
   }
 
+  async update(id: bigint, dto: UpdateProductDto) {
+    await this.validateParams(dto, id);
+
+    const product = await this.prisma.$transaction(async (tx) => {
+      const product = await tx.product.update({
+        where: {
+          id,
+          deletedAt: null,
+        },
+        data: {
+          name: dto.name,
+          number: dto.number,
+          shelfStatus: dto.shelfStatus,
+          thumbnail: dto.thumbnail,
+          pictures: dto.pictures,
+          description: dto.description,
+          categories:
+            dto.categoryIds && dto.categoryIds.length > 0
+              ? {
+                  connect: dto.categoryIds.map((id) => ({ id })),
+                }
+              : undefined,
+          operatedBy: this.request.user.id,
+        },
+      });
+      if (dto.skus && dto.skus.length > 0) {
+        await tx.productSku.updateMany({
+          where: {
+            productId: id,
+            deletedAt: null,
+          },
+          data: {
+            deletedAt: new Date(),
+          },
+        });
+        await tx.product.update({
+          where: {
+            id,
+            deletedAt: null,
+          },
+          data: {
+            skus: {
+              createMany: {
+                data: dto.skus.map((sku) => ({
+                  name: sku.name,
+                  costPrice: sku.costPrice,
+                  price: sku.price,
+                  stock: sku.stock,
+                  operatedBy: this.request.user.id,
+                })),
+              },
+            },
+          },
+        });
+      }
+
+      return product;
+    });
+
+    return this.detail(product.id);
+  }
+
   async detail(id: bigint) {
     const product = await this.prisma.product.findUnique({
       where: {
@@ -71,12 +137,18 @@ export class ProductsService {
       },
       include: {
         skus: {
+          where: {
+            deletedAt: null,
+          },
           omit: {
             deletedAt: true,
             operatedBy: true,
           },
         },
         categories: {
+          where: {
+            deletedAt: null,
+          },
           omit: {
             deletedAt: true,
             operatedBy: true,
@@ -85,5 +157,41 @@ export class ProductsService {
       },
     });
     return product;
+  }
+
+  private async validateParams(
+    params: CreateProductDto | UpdateProductDto,
+    id?: bigint,
+  ) {
+    if (params.number) {
+      const existNumber = await this.prisma.product.findFirst({
+        where: {
+          number: params.number,
+          deletedAt: null,
+          id: id ? { not: id } : undefined,
+        },
+      });
+      if (existNumber) {
+        throw new ConflictException(
+          `number|${ERROR_CONFLICT_MESSAGE_CODE.DUPLICATE_SLUG}`,
+        );
+      }
+    }
+
+    if (params.categoryIds && params.categoryIds.length > 0) {
+      const categories = await this.prisma.productCategory.findMany({
+        where: {
+          id: {
+            in: params.categoryIds,
+          },
+          deletedAt: null,
+        },
+      });
+      if (!categories || categories.length !== params.categoryIds.length) {
+        throw new BadRequestException(
+          `categoryIds|${ERROR_CONFLICT_MESSAGE_CODE.INVALID_RELATION}`,
+        );
+      }
+    }
   }
 }
